f79d69b251baed72f900ee9881cf3905d5aeeb3b
Ainika <aina.kostina@gmail.com>
2021-01-09 12:06:01 +0200

    Changed LF, transported components to Home page

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,7 @@
   <div class="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <h1 class="app">test</h1>
   </div>
-  <Header/>
-  <Search/>
-  <Card/>
   <router-view/>
 </template>
 
@@ -14,15 +10,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Header from './components/header/header.vue';
-import Search from './components/search/search.vue';
-import Card from './components/card/card.vue';
 
 const App = defineComponent({
   components: {
-    Header,
-    Search,
-    Card,
   },
 });
 
