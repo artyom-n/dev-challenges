da22b7cb415804212167b6db0177a72cbeddc044
Ainika <aina.kostina@gmail.com>
2021-01-09 11:16:53 +0200

    Added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1 class="app">test</h1>
+  <div class="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <h1 class="app">test</h1>
+  </div>
+  <router-view/>
 </template>
 
 <style src="./app.scss" lang="scss" scoped></style>
