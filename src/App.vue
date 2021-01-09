7d7f0079ce022d07023938f1d37f276ff1679e24
WorkPrompt <workprompt@gmail.com>
2021-01-09 12:08:32 +0200

    added files

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,16 @@
   <div class="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <h1 class="app">test</h1>
   </div>
-  <Header/>
-  <Search/>
-  <Card/>
   <router-view/>
 </template>
-
 <style src="./app.scss" lang="scss" scoped></style>
-
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Header from './components/header/header.vue';
-import Search from './components/search/search.vue';
-import Card from './components/card/card.vue';
 
 const App = defineComponent({
   components: {
-    Header,
-    Search,
-    Card,
   },
 });
-
 export default App;
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <h1>This is home page</h1>
+    <Header />
+    <Search />
+    <Card />
   </div>
 </template>
-
 <script>
+import Header from '../components/header/header.vue';
+import Search from '../components/search/search.vue';
+import Card from '../components/card/card.vue';
 
 export default {
   name: 'Home',
   components: {
+    Header,
+    Search,
+    Card,
   },
 };
 </script>
